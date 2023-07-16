5568da622de7127275d5f62c51371b90ce4f5e93
Armağan Taş <65099385+armagantas@users.noreply.github.com>
2023-07-16 21:18:56 +0300

    Commit

---
+++ src/components/trustedBy/TrustedBy.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./TrustedBy.scss";
+
+const TrustedBy = () => {
+  return (
+    <div className="trustedBy">
+      <div className="container">
+        <span>Trusted By:</span>
+        <img src="../../public/img/facebookTrusted.png" alt="" />
+        <img src="../../public/img/googleTrusted.png" alt="" />
+        <img src="../../public/img/netflixTrusted.png" alt="" />
+        <img src="../../public/img/p&gTrusted.png" alt="" />
+        <img src="../../public/img/paypalTrusted.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default TrustedBy;
