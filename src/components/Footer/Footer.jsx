9a5f4f8ce774f4bafc5fa517bea605a1a135b57c
Armağan Taş <65099385+armagantas@users.noreply.github.com>
2023-07-18 01:48:30 +0300

    Commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -77,9 +77,30 @@ const Footer = () => {
             <span>Sitemap</span>
           </div>
         </div>
+        <hr />
         <div className="bottom">
-          <div className="left"></div>
-          <div className="right"></div>
+          <div className="left">
+            <h2>fiverr</h2>
+            <span>© Liverr International Ltd. 2023</span>
+          </div>
+          <div className="right">
+            <div className="social">
+              <img src="/img/twitter.png" alt="" />
+              <img src="/img/facebook.png" alt="" />
+              <img src="/img/linkedin.png" alt="" />
+              <img src="/img/pinterest.png" alt="" />
+              <img src="/img/instagram.png" alt="" />
+            </div>
+            <div className="link">
+              <img src="./img/language.png" alt="" />
+              <span>English</span>
+            </div>
+            <div className="link">
+              <img src="./img/coin.png" alt="" />
+              <span>USD</span>
+            </div>
+            <img src="./img/accessibility.png" alt="" />
+          </div>
         </div>
       </div>
     </div>
